471d142192ea7773e18785cd59006bb9b6f8c795
CarolinaQuituisaca <65557178+CarolinaQuituisaca@users.noreply.github.com>
2020-08-07 02:40:42 -0500

    modificar11

--- config/database.php
+++ config/database.php
@@ -68,16 +68,16 @@ return [
             'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'Final'),
+            'username' => env('DB_USERNAME', 'posgres'),
+            'password' => env('DB_PASSWORD', '1234'),
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
             'schema' => 'public',
             'sslmode' => 'prefer',
         ],
-
+        
         'sqlsrv' => [
             'driver' => 'sqlsrv',
             'url' => env('DATABASE_URL'),
